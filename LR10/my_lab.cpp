c24d6e98a1b2afb39e20b210f533d58b1c1265a7
logerse <kotovcod@gmail.com>
2020-03-22 11:46:22 +0200

    some fix's

--- LR10/my_lab.cpp
+++ LR10/my_lab.cpp
@@ -1,66 +1,42 @@
 #include "my_lab.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h> // malloc free
 
 #define NEND(c) ((c) != EOF && (c) != '\n')
-
+#define FALSE 0
+#define TRUE  0
 
 typedef struct {
 	size_t size;
-	char content[0];
+	char *content;
 } line_t;
 
 
 void getFileName(char *buf)
 {
-  int i = 0;
-  int c = getchar();
-  for(; i<BUF_SIZE-1; i++)
-  {
-    if( !NEND(c) )
-      break;
-    buf[i] = c;
-    c = getchar();
-  }
-  buf[i] = 0;
+    int i = 0;
+    int c = getchar();
+    for(; i<BUF_SIZE-1; i++)
+    {
+        if( !NEND(c) )
+            break;
+        buf[i] = c;
+        c = getchar();
+    }
+    buf[i] = 0;
 };
 
 
 int getLineLen(void)
 {
-  int lineLen = 100;
-  printf("[*] Input line length [default: 100]: ");
-  if( scanf("%d", &lineLen) == 0 || lineLen < 1)
+    int lineLen = 100;
+    printf("[*] Input line length [default: 100]: ");
+    if( scanf("%d", &lineLen) == 0 || lineLen < 1)
 	return 100;
-  return lineLen;
+    return lineLen;
 };
-
-/*
-int getMaxLen(FILE  *file)
-{
-  int max = 0;
-  int count = 0;
-  int c = fgetc(file);
-  while( !feof(file) )
-  {
-    if(c != '\n')
-      count++;
-    else
-    {
-      if(count > max)
-        max = count;
-      count = 0;
-    }
-
-    c = fgetc(file);
-  }
-  if(count > max)
-    max = count;
-
-  return max;
-};
-*/
 
 
 static void
@@ -75,40 +51,55 @@ getFileSize(FILE *file, const int* fileSize)
 static const char*
 loadInputFile(FILE *_in)
 {
-	if(_in == NULL) return NULL;
+	if(_in == NULL) goto ERROR;
 	int fileSize = 0;
 	getFileSize(_in, &fileSize);
-	if( fileSize == 0 ) return NULL;
+	if( fileSize == 0 ) {
+		fclose(_in);
+		goto ERROR;
+	};
 	
-	char c;
 	char *buffer = (char*)  malloc(fileSize + 1);
 
 	for(int $pos=0; $pos < fileSize; $pos++)
-		buffer[$pos] = (isspace( c = fgetc(_in) )) c:' ';
+		buffer[$pos] = fgetc(_in);
 
-	buffer[fileSize - 1] = 0;
+	buffer[fileSize] = -1;
 	
 	return (const char*) buffer;
+
+ERROR:
+    puts("[X] Corrupted input file.");
+	exit(-1);	
 };
 
 
 void copyFile(FILE *_out, FILE *_in, const int lineLen)
 {
-  const char *inputBuffer = loadInputFile(_in);
-  if( inputBuffer == NULL )
-  {
-    puts("[X] Corrupted input file.");
-	exit(-1);	
-  };  
-  fclose(_in);
+    const char *inputBuffer = loadInputFile(_in);
+    fclose(_in);
 
-  for(int i=0; inputBuffer[i] != 0 ; i++)
-  {
-	if(inputBuffer[i] != 0)
+    int curLen = 0;
+	line_t lineArray[1024] = {0,NULL}; // NOTE: Replace const-size initialization
+	char *lastDelimiter = NULL;
+    char *beginLine = inputBuffer;
+    while( beginLine[curLen] != -1 )
+    {
+    	while( isspace(beginLine[curLen]) == FALSE && curLen <= lineLen )
+			curLen++;
+	    
+		if(lineLen=>curLen)
+			lastDelimiter = beginLine[curLen];
+		else {
+			if( lastDelimiter != NULL ) {
+				*lastDelimiter = '\x00';
+				
+			}
+			else
+		}
+    };
 
-  };  
-
-  free(inputBuffer);
+    free(inputBuffer);
 };
 
 // #---------------------------------#
