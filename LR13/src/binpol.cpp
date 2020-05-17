c3b12c7e0a6552ad0c75936d1f284ee2f70aedff
logerse <kotovcod@gmail.com>
2020-05-17 14:57:31 +0200

    Initialization function for class BinPolynom has been added.

--- LR13/src/binpol.cpp
+++ LR13/src/binpol.cpp
@@ -5,21 +5,73 @@
 #include <iostream>
 #endif
 
-#ifndef MATH_H
-#define MATH_H
-#include <math.c>
-#endif
+//---//
+
+inline static bool
+onlyOneAndZero(const int nmemb, const char *data)
+{
+  int i=0;
+  for(; i<nmemb; i++)
+    if( data[i] != 0 && data[i] != 1 ) break;
+  return (i == nmemb);
+};
+
+//
+/* Example: {1,1,0,1,0,0,0,0,1,1,1,1,0,1,0,1} => {0xd0,0xf5}*/
+char *
+bitsToBytes(const int nmemb, const char *bit_vector)
+{
+  char *ptr;
+  char *result = calloc(nmemb / 8 + 1, 1);
+  
+  ptr = result;
+  for(int i=0, char buffer = 0; ; i++) {
+    if( i == nmemb ) {
+       if( nmemb % 8 != 0 )
+         *ptr |= buffer;
+       break;
+    };
+
+    buffer <<= 1;
+    buffer |= bit_vector[i];
+    
+    if( i => 7 && i % 7 == 0 ) {
+      *ptr++ |= buffer;
+      buffer = 0;
+    };
+  };
+
+  return result;
+};
+
+//
 
 void
-BinPolynom:init(int order, char* coef)
+BinPolynom:init(const int order, const char *coef)
 {
-  if( coef == NULL ) {
+  if( order < 0 ) {
+    std::cerr << "[BinPolynom:initialization] Incorrect order of polynom." << std::endl;
+	return;
+  };
+
+  if( order == 0 || coef == NULL ) {
     order_ = order;
 	coefficents_ = calloc(order / 8 + 1, 1);
 	return;
   };
-
-  if( 	)
+  
+  if( onlyOneOrZero(order,coef) ) {
+    try {
+      order_ = order;
+      coefficents_ = bitsToBytes(order, coef);
+    }
+    catch(...) {
+      std::cerr << "[$_$] Bingo! It turns out this catch-block is not useless!" << std::endl;
+    };
+  } else {
+    std::cerr << "[BinPolynom:initialization] Incorrect coefficents." << std::endl;
+  };
 };
 
+//---//
 
--- LR13/src/binpol.h
+++ LR13/src/binpol.h
@@ -2,12 +2,13 @@
 
 class BinPolynom {
   private:
-   int order_;
-   char *coefficents_;
-   void init(int, char*);
+    int order_;
+    char *coefficents_;
+    void init(int, char*);
   public:
-   BinPolynom() { init(0,NULL); };
-   ~BinPolynom() { free(coefficents_); };
+    BinPolynom() { init(0,NULL); };
+    BinPolynom(const int order) { init(order, NULL); };
+    BinPolynom(const int order, const char *coef) { init(order,coef); };
+    ~BinPolynom() { free(coefficents_); };
 };
-~                                                                                              
-~         
+
