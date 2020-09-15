b8baad9438ae350e862e9870f0c18b4c5ad7968d
Kirill Kotov <k.kotov@dev.local>
2020-09-15 10:15:43 +0300

    Exceptions

---
+++ LR17/src/vector/vectorExceptions.h
@@ -0,0 +1,12 @@
+#ifndef STRING_H
+#define STRING_H
+#include <string.h>
+#endif
+
+class MyException {
+private:
+	char message[256];
+public:
+	MyException(const char *msg) : message{0} { strcpy(message, msg); };
+	const char* get() const { return message; }; 
+};
